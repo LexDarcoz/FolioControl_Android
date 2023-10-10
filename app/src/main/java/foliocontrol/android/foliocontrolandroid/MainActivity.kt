d3a99d4b6796eec885b5e5257bc2a0b103855475
Alexander Schatteman <Alexander.schatteman@student.hogent.be>
2023-10-10 23:38:47 +0200

    Init

---
+++ app/src/main/java/foliocontrol/android/foliocontrolandroid/MainActivity.kt
@@ -0,0 +1,116 @@
+package foliocontrol.android.foliocontrolandroid
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import foliocontrol.android.foliocontrolandroid.ui.theme.FolioControl_AndroidTheme
+
+data class BottomNavigationItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val onClick: () -> Unit
+
+)
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FolioControl_AndroidTheme {
+                val items = listOf(
+                    BottomNavigationItem(
+                        title = "Home",
+                        selectedIcon = Icons.Filled.Home,
+                        unselectedIcon = Icons.Outlined.Home,
+                        onClick = { /*TODO*/ }
+
+                    ),
+                    BottomNavigationItem(
+                        title = "Search",
+                        selectedIcon = Icons.Filled.Search,
+                        unselectedIcon = Icons.Outlined.Search,
+                        onClick = { /*TODO*/ }
+
+                    ),
+                    BottomNavigationItem(
+                        title = "Profile",
+                        selectedIcon = Icons.Filled.AccountCircle,
+                        unselectedIcon = Icons.Outlined.AccountCircle,
+                        onClick = { /*TODO*/ }
+
+                    ),
+                    BottomNavigationItem(
+                        title = "Settings",
+                        selectedIcon = Icons.Filled.Settings,
+                        unselectedIcon = Icons.Outlined.Settings,
+                        onClick = { /*TODO*/ }
+
+                    )
+                )
+                var selectedIcon by rememberSaveable {
+                    mutableStateOf(0)
+                }
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.secondary
+
+                ) {
+                    Scaffold(
+                        Modifier.background(MaterialTheme.colorScheme.secondary),
+                        bottomBar = {
+                            NavigationBar {
+                                items.forEachIndexed { index, item ->
+                                    NavigationBarItem(
+                                        selected = selectedIcon == index,
+                                        onClick = {
+                                            selectedIcon = index
+                                            // navController.navigate(item.title)
+                                        },
+                                        label = {
+                                            Text(text = item.title)
+                                        },
+                                        alwaysShowLabel = false,
+                                        icon = {
+                                            item.unselectedIcon
+                                        }
+
+                                    )
+                                }
+                            }
+                        }
+                    ) {
+                    }
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/foliocontrol/android/foliocontrolandroid/ui/theme/Type.kt
+++ app/src/main/java/foliocontrol/android/foliocontrolandroid/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package foliocontrol.android.foliocontrol_android.ui.theme
+package foliocontrol.android.foliocontrolandroid.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
